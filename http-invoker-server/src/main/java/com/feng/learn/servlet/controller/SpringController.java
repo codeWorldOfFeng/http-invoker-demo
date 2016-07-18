509d76193268f3886954cef8a8715e7fae6cea41
feng <feng@feng-pc>
2016-07-18 14:28:16 +0800

    添加请求信息demo

--- http-invoker-server/src/main/java/com/feng/learn/servlet/controller/SpringController.java
+++ http-invoker-server/src/main/java/com/feng/learn/servlet/controller/SpringController.java
@@ -7,19 +7,47 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/sc")
+@RequestMapping(value = "/sc")
 public class SpringController {
-	
-	@RequestMapping(value="/get")
-	public void get(HttpServletRequest req, HttpServletResponse resp) {
-		resp.setContentType("text/html;charset=utf-8");
-		try {
-			resp.getWriter().println("Hello spring Mvc. 你好。。");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public void get(HttpServletRequest req, HttpServletResponse resp) {
+	System.out.println(req.getContextPath()); // /jetty
+
+	System.out.println(req.getRequestURI()); // /jetty/sc/get
+	System.out.println(req.getRequestURL()); // http://localhost:8080/jetty/sc/get
+
+	// System.out.println(req.getServletContext());
+	System.out.println(req.getServletPath());
+	System.out.println(req.getServerName());
+	System.out.println(req.getServerPort());
+
+	System.out.println(req.getLocalAddr());
+	System.out.println(req.getLocalName());
+	System.out.println(req.getLocalPort());
+
+	System.out.println(req.getRemoteAddr());
+	System.out.println(req.getRemotePort());
+	System.out.println(req.getRemoteHost());
+	resp.setContentType("text/html;charset=utf-8");
+	try {
+	    resp.getWriter().println("Hello spring Mvc. 你好。。(GET)");
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
+	resp.setContentType("text/html;charset=utf-8");
+	try {
+	    resp.getWriter().println("Hello spring Mvc. 你好。。(POST)");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
 
 }
